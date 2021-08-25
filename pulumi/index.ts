a910a0848df71ee242b8375a69aba6f4cde4e803
Andrei <andrei@boardme.io>
2021-08-25 20:09:39 +0200

    Pulumi callback and API call

--- pulumi/index.ts
+++ pulumi/index.ts
@@ -2,6 +2,7 @@ import * as pulumi from '@pulumi/pulumi';
 import * as resources from '@pulumi/azure-native/resources';
 import * as storage from '@pulumi/azure-native/storage';
 import * as web from '@pulumi/azure-native/web';
+import axios from 'axios';
 
 // Create an Azure Resource Group
 // const resourceGroup = new resources.ResourceGroup("resourceGroup");
@@ -35,7 +36,9 @@ const helloApp = new web.WebApp('pulumiHelloApp', {
   httpsOnly: true,
 });
 
-export const helloEndpoint = pulumi.interpolate`https://${helloApp.defaultHostName}/hello`;
+export const helloEndpoint = pulumi.interpolate`https://${helloApp.defaultHostName}`;
+
+console.log('Testing if console.log works...');
 
 // Create an Azure resource (Storage Account)
 const storageAccount = new storage.StorageAccount('pulumistorage', {
@@ -53,3 +56,38 @@ const storageAccountKeys = pulumi
     storage.listStorageAccountKeys({ resourceGroupName, accountName })
   );
 export const primaryStorageKey = storageAccountKeys.keys[0].value;
+
+pulumi.all([helloEndpoint, helloApp.name]).apply(([endpoint, appName]) => {
+  console.log("No idea what's happening here: ", endpoint, appName);
+
+  console.log('Is pulumi dry run?: ', pulumi.runtime.isDryRun());
+
+  axios
+    .get('http://jsonplaceholder.typicode.com/posts?_limit=1')
+    .then((response) => {
+      // handle success
+      console.log(response.data);
+      console.log('Hello endpoint:', endpoint);
+      console.log('App service name:', appName);
+    })
+    .catch((error) => {
+      // handle error
+      console.log(error);
+    });
+});
+
+// axios
+//   .get('http://jsonplaceholder.typicode.com/posts')
+//   .then((response) => {
+//     // handle success
+//     console.log(response);
+//     console.log(
+//       'Hello endpoint:',
+//       pulumi.interpolate`${helloApp.defaultHostName}`
+//     );
+//     console.log('App service name:', pulumi.interpolate`${helloApp.name}`);
+//   })
+//   .catch((error) => {
+//     // handle error
+//     console.log(error);
+//   });
