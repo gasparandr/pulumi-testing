4102a0e6b7b432e2643e879ac35fad19756e5bc9
Andrei <andrei@boardme.io>
2021-08-25 18:02:05 +0200

    Deploy app service with docker image

--- pulumi/index.ts
+++ pulumi/index.ts
@@ -1,10 +1,41 @@
 import * as pulumi from '@pulumi/pulumi';
 import * as resources from '@pulumi/azure-native/resources';
 import * as storage from '@pulumi/azure-native/storage';
+import * as web from '@pulumi/azure-native/web';
 
 // Create an Azure Resource Group
 // const resourceGroup = new resources.ResourceGroup("resourceGroup");
 const resourceGroup = { name: 'CCBSandbox' };
+
+const plan = new web.AppServicePlan('PulumiPlan', {
+  resourceGroupName: resourceGroup.name,
+  kind: 'Linux',
+  reserved: true,
+  sku: {
+    name: 'B1',
+    tier: 'Basic',
+  },
+});
+
+const dockerImage = 'mcr.microsoft.com/oss/nginx/nginx:1.15.5-alpine';
+
+const helloApp = new web.WebApp('helloApp', {
+  resourceGroupName: resourceGroup.name,
+  serverFarmId: plan.id,
+  siteConfig: {
+    appSettings: [
+      {
+        name: 'WEBSITES_ENABLE_APP_SERVICE_STORAGE',
+        value: 'false',
+      },
+    ],
+    alwaysOn: true,
+    linuxFxVersion: `DOCKER|${dockerImage}`,
+  },
+  httpsOnly: true,
+});
+
+export const helloEndpoint = pulumi.interpolate`https://${helloApp.defaultHostName}/hello`;
 
 // Create an Azure resource (Storage Account)
 const storageAccount = new storage.StorageAccount('sa', {
