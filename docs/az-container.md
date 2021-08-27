33a3b5194f1df09710aa53fad10d87197c233869
Andrei Gaspar <andrei@boardme.io>
2021-08-27 13:35:54 +0200

    Revert "Testing passing value between job steps"
    This reverts commit 7937bc48560b3d43952191d8e93c9f6770b0e399.

--- docs/az-container.md
+++ docs/az-container.md
@@ -17,4 +17,3 @@ az acr build --registry PulumiMockRegistry --image pulumi-mock-image .
 - [Deploy Docker Image to App Service](https://github.com/pulumi/examples/blob/master/azure-ts-appservice-docker/index.ts)
 - [Get Container Registry (Not okay - Use the @pulumi/azure_native package instead)](https://www.pulumi.com/docs/reference/pkg/azure/containerservice/getregistry/)
 - [Upload React to Azure Storage Account](https://medium.com/bb-tutorials-and-thoughts/how-to-host-a-react-static-website-on-azure-438e0a915295)
-- [Sharing data between GitHub Actions](https://www.edwardthomson.com/blog/github_actions_15_sharing_data_between_steps.html)
--- pulumi/handlers/up-command-handler.ts
+++ pulumi/handlers/up-command-handler.ts
@@ -1,6 +1,5 @@
 import axios from 'axios';
 import { remoteURL } from '../config';
-import { exec } from 'child_process';
 
 interface UpCommandParams {
   appName: string;
@@ -8,20 +7,6 @@ interface UpCommandParams {
 }
 
 export const upCommandHandler = ({ appName, endpoint }: UpCommandParams) => {
-  exec(
-    `echo "::set-env name=APP_ENDPOINT::${endpoint}"`,
-    (err: any, stdout: any, stderr: any) => {
-      if (err) {
-        //some err occurred
-        console.error(err);
-      } else {
-        // the *entire* stdout and stderr (buffered)
-        console.log(`stdout: ${stdout}`);
-        console.log(`stderr: ${stderr}`);
-      }
-    }
-  );
-
   axios
     .get(
       `${remoteURL}/api/createAppConfig?branchName=${process.env.BRANCH_NAME}&appName=${appName}`
